3baafca6cb34717bd66a8fcf8ef8a3529852466c
David Ingram <david@ingramaviation.com>
2019-08-15 17:09:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,10 +2,12 @@ def my_collect(array)
   i = 0
   array2 =[]
   array3 =[]
+  array4 =[]
   while i < array.length
     yield array[i]
     array2 << array[i].upcase
     array3 << array[i].capitalize
+    array4 << array[i].
     i = i + 1
 end
     array2 
