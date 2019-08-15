c6175aed8a4b3c66cc28b20ca57f2c3160ce3deb
David Ingram <david@ingramaviation.com>
2019-08-15 17:10:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -7,11 +7,12 @@ def my_collect(array)
     yield array[i]
     array2 << array[i].upcase
     array3 << array[i].capitalize
-    array4 << array[i].
+    array4 << array[i].split(" ").first
     i = i + 1
 end
     array2 
     array3
+    array4
   
   
   
