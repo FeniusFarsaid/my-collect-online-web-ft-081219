a0638a578b19615f84871df6ef712d7f0875c280
David Ingram <david@ingramaviation.com>
2019-08-15 17:14:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,17 +2,15 @@ def my_collect(array)
   i = 0
   array2 =[]
   array3 =[]
-  array4 =[]
   while i < array.length
     yield array[i]
     array2 << array[i].upcase
-    #array3 << array[i].
-    array4 << array[i].split(" ").first
+    array3 << array[i].split(" ").first
     i = i + 1
 end
     array2 
-    #array3
-    array4
+    array3
+    
   
   
   
