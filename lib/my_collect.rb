13bff8dc13bb80189bec1252fb2edd7f325275f2
David Ingram <david@ingramaviation.com>
2019-08-15 17:53:01 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(array)
-  i = 0
+  x = 0
   array2 =[]
-  while i < array.length
-    array2 << yield(array[i])
-    i = i + 1
+  while x < array.length
+    array2 << yield(array[x])
+    x = x + 1
 end
     array2 
 end
